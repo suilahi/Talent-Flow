8f9c65dd0b0a71fa4078eb356e8af116913805f0
yassine ELANIQ <yassaniq@gmail.com>
2025-03-06 12:51:10 +0000

    creation de la candidatures avec le CRUD

--- src/main/java/Candidature/Candidate/Model/CandidateModel.java
+++ src/main/java/Candidature/Candidate/Model/CandidateModel.java
@@ -1,4 +1,44 @@
 package Candidature.Candidate.Model;
 
-public class CandidateModel {
+import authentification.Model.User;
+
+
+public class CandidateModel extends User {
+
+    private String cv ;
+
+    public CandidateModel(int id, String name, String email, String password, UserRole role, String cv) {
+        super(id, name, email, password, role);
+        this.cv = cv;
+    }
+
+    public CandidateModel(String name, String email, String password, UserRole userRole, String cv) {
+        super(name, email, password, userRole != null ? userRole : UserRole.CANDIDATE);
+        this.cv = cv;
+    }
+
+
+    public CandidateModel(String cv) {
+        this.cv = cv;
+    }
+
+    public CandidateModel() {
+    }
+
+    public String getCv() {
+        return cv;
+    }
+
+    public void setCv(String cv) {
+        this.cv = cv;
+    }
+
+    @Override
+    public String toString() {
+        return "CandidateModel{" +
+                "cv='" + cv + '\'' +
+                '}';
+    }
 }
+
+
