a2e706cfaad1f420ab4451fba6d7fe3f3a4ab09c
suilahi <suilahizakaria@gmail.com>
2025-03-06 11:43:21 +0000

    changes in dao

--- src/main/java/authentification/Dao/UserDao.java
+++ src/main/java/authentification/Dao/UserDao.java
@@ -13,4 +13,16 @@ public class UserDao {
     public UserDao(Connection connection) {
         this.connection = connection;
     }
+    public void addUser(User user) throws SQLException {
+        String query = "INSERT INTO users (name,  email,Mot_de_passe, role) VALUES ( ?, ?, ?, ?)";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, user.getName());
+            stmt.setString(2, user.getEmail());
+            stmt.setString(3, user.getPassword());
+            stmt.setString(4, String.valueOf(user.getRole()));
+            stmt.executeUpdate();
+        }
+    }
+
 }
