7653bbaa8529746df61fb796a212e7d027019620
Youssef Ait Rais <copybuisness007@gmail.com>
2025-03-07 14:21:14 +0100

    updates in status Recruteur

--- src/main/java/Candidature/Recruteur/Model/OfferStatusModel.java
+++ src/main/java/Candidature/Recruteur/Model/OfferStatusModel.java
@@ -17,7 +17,6 @@ public class OfferStatusModel {
         this.updatedAt = updatedAt;
     }
 
-    // Getters & Setters
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
 
